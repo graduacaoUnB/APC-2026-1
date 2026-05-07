8477ec56ab7ac461751503a135e3aff84007cc69
Marcos Torres <marcmam2@gmail.com>
2026-05-06 21:36:58 -0300

    Update calculadora.py

--- semana09/calculadora.py
+++ semana09/calculadora.py
@@ -1,1 +1,13 @@
 primeira reimplementação em Python
+
+## código ou arquivo do simulador (OctoStudio, Python, Tinkercad)
+- 
+
+## imagens ou capturas de tela
+- 
+
+## documentação descrevendo o algoritmo
+- 
+
+## reflexão breve sobre o que foi aprendido
+- 
